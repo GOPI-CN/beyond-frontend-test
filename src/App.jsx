e371d8b36715e9e1aca977c208439474bcdd16d1
Gopi-React <gopi@riotinfomedia.com>
2026-02-26 10:43:16 +0530

    Initial clean Vite React setup with routing

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return <h2>Home Page</h2>;
+}
+
+function About() {
+  return <h2>About Page</h2>;
+}
+
+export default function App() {
+  return (
+    <div>
+      <nav style={{ marginBottom: "20px" }}>
+        <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
